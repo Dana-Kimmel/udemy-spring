1a8247e3c8f81a6042b7b7f734582203421b8687
Dana Kimmel <dear.prudence34@gmail.com>
2021-08-18 21:55:27 -0400

    finally fixed the dang hal-browser weeeee:)

--- springboot-in-10-steps/src/test/java/com/in28minutes/springboot/basics/springbootin10steps/SpringbootIn10StepsApplicationTests.java
+++ springboot-in-10-steps/src/test/java/com/in28minutes/springboot/basics/springbootin10steps/SpringbootIn10StepsApplicationTests.java
@@ -1,13 +1,16 @@
 package com.in28minutes.springboot.basics.springbootin10steps;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringbootIn10StepsApplicationTests {
+public class SpringbootIn10StepsApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
